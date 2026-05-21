d1b1bbe213ade286b68e0c4e5407a3a9648a4642
Jining Liu <hello@jining.dev>
2026-05-21 18:56:08 -0500

    manifest

---
+++ generate-manifest.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+tmp=$(mktemp)
+
+find . -type f -name "*.txt" | while read -r file; do
+  file="/${file#./}"
+  dir=$(dirname "$file")
+
+  title=$(sed -n '1p' ".$file")
+  subtitle=$(sed -n '2p' ".$file")
+  date=$(sed -n '3p' ".$file")
+
+  jq -n \
+    --arg dir "$dir" \
+    --arg path "$file" \
+    --arg title "$title" \
+    --arg subtitle "$subtitle" \
+    --arg date "$date" \
+    '{
+      path: $dir,
+      file: {
+        path: $path,
+        title: $title,
+        subtitle: $subtitle,
+        date: $date
+      }
+    }'
+done > "$tmp"
+
+jq -s '
+{
+  version: 1,
+  directories:
+    group_by(.path)
+    | map({
+        path: .[0].path,
+        files: map(.file)
+      })
+}
+' "$tmp" > manifest.json
+
+rm "$tmp"
